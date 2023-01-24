501a0fe862740fabbf41f99e80053e90bd673d9c
unknown <threezinedine@gmail.com>
2023-01-24 21:16:26 +0700

    add eslint for cypress e2e tests

--- cypress/e2e/LoginPage.cy.ts
+++ cypress/e2e/LoginPage.cy.ts
@@ -1,5 +1,5 @@
 describe("Login page testing", () => {
-    it('should have the login page', () => {
+    it("should have the login page", () => {
         cy.visit("http://localhost:3000/login")
     })
 })
