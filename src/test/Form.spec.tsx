4b8def9175c64f57dac82a509ac85c0d9ebd76e8
unknown <threezinedine@gmail.com>
2023-01-24 21:51:55 +0700

    the form component that can enter the value and then call the onSubmit when the submit button is clickec

--- src/test/Form.spec.tsx
+++ src/test/Form.spec.tsx
@@ -3,6 +3,7 @@ import {
     render,
     screen,
 } from '@testing-library/react'
+import userEvent from "@testing-library/user-event"
 
 import { 
     Form,
@@ -29,5 +30,41 @@ describe('The <Form /> Component Testing', () => {
         expect(screen.getByTestId("username")).toBeInTheDocument()
         expect(screen.getByTestId("password")).toBeInTheDocument()
         expect(screen.getByTestId("loginBtn")).toBeInTheDocument()
+    })
+
+    it('should run the onSubmit component when the loginBtn is clicked', () => {
+        const onSubmitStub = jest.fn()
+
+        render(
+            <Form 
+                fields={[
+                    {
+                        name: "username",
+                        label: "Username",
+                    },
+                    {
+                        name: "password",
+                        label: "Password",
+                    }
+                ]}
+                onSubmit={onSubmitStub}
+            />
+        )
+
+        userEvent.type(screen.getByTestId("username"), "threezinedine")
+        userEvent.type(screen.getByTestId("password"), "threezinedine")
+        userEvent.click(screen.getByTestId("loginBtn"))
+    
+        expect(onSubmitStub).toHaveBeenCalledWith([
+            {
+                name: "username",
+                value: "threezinedine"
+            },
+            {
+                name: "password",
+                value: "threezinedine"
+            }
+        ])
+
     })
 })
