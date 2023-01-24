d4c1a31caf25056ce450c686cd72f08ac2a7fb4e
unknown <threezinedine@gmail.com>
2023-01-24 21:21:25 +0700

    add e2e test for login page

--- cypress/e2e/LoginPage.cy.ts
+++ cypress/e2e/LoginPage.cy.ts
@@ -1,5 +1,14 @@
 describe("Login page testing", () => {
     it("should have the login page", () => {
         cy.visit("http://localhost:3000/login")
+        
+        cy.contains("[data-testid=\"username\"]")
+            .type("threezinedine")
+
+        cy.contains("[data-testid=\"password\"]")
+            .type("threezinedine")
+
+        cy.contains("[data-testid=\"login\"]")
+            .click()
     })
 })
