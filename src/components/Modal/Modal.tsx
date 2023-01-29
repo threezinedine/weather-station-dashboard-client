f9111d30a8f2af5aa7e1fe1fca2cf429e35d6c6e
unknown <threezinedine@gmail.com>
2023-01-29 21:22:42 +0700

    pass test for not displaying the content at the first time of the modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,10 +1,15 @@
 import React from "react"
 
+import ModalProps from "./ModalProps"
 
-const Modal: React.FC = () => {
+
+const Modal: React.FC<ModalProps> = ({
+    children,
+    visible = false,
+}) => {
     return (
         <div>
-
+            { visible && children }
         </div>
     )
 }
---
+++ src/components/Modal/ModalProps.ts
@@ -0,0 +1,7 @@
+import React from "react"
+
+
+export default interface ModalProps {
+    children: React.ReactNode
+    visible?: boolean
+}
