9f4b5d2c9ed3eb6656edf82c5c1c5440b27379a0
unknown <threezinedine@gmail.com>
2023-01-29 14:02:21 +0700

    pass test for on Click function

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,9 +5,12 @@ import ButtonProps from "./ButtonProps"
 
 const Button: React.FC<ButtonProps> = ({
     children,
+    onClick,
 }) => {
     return (
-        <div>
+        <div
+            onClick={onClick}
+        >
             { children }
         </div>
     )
--- src/components/Button/ButtonProps.ts
+++ src/components/Button/ButtonProps.ts
@@ -3,4 +3,5 @@ import React from "react"
 
 export default interface ButtonProps {
     children: React.ReactNode
+    onClick: () => void
 }
