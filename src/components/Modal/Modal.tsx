71f61e38384f6a23f98be9853b17428dee94da3f
unknown <threezinedine@gmail.com>
2023-01-29 21:30:27 +0700

    pass test that the modal should have cancel button and submit button

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,13 @@
 import React from "react"
 
 import ModalProps from "./ModalProps"
+import { 
+    Button, 
+} from "components"
+import {
+    MODAL_CANCEL_BUTTON_TEST_ID, 
+    MODAL_SUBMIT_BUTTON_TEST_ID,
+} from "const"
 
 
 const Modal: React.FC<ModalProps> = ({
@@ -10,6 +17,16 @@ const Modal: React.FC<ModalProps> = ({
     return (
         <div>
             { visible && children }
+            <Button
+                data-testid={MODAL_CANCEL_BUTTON_TEST_ID}
+            >
+                Cancel
+            </Button>
+            <Button
+                data-testid={MODAL_SUBMIT_BUTTON_TEST_ID}
+            >
+                Submit
+            </Button>
         </div>
     )
 }
