a620eaa643f698c42891908abccce2bd770af20a
unknown <threezinedine@gmail.com>
2023-01-29 14:01:42 +0700

    add test for calling the onClick method when the button is clicked

--- src/test/Button.spec.tsx
+++ src/test/Button.spec.tsx
@@ -3,6 +3,7 @@ import {
     render,
     screen,
 } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 
 import { 
     Button,
@@ -11,10 +12,13 @@ import {
 
 describe("Testing button", () => {
     const buttonLabel = "Button"
+    const mockOnClickFunction = jest.fn()
 
     const renderButton = (label: string) => {
         render(
-            <Button>
+            <Button
+                onClick={mockOnClickFunction}
+            >
                 { label }
             </Button>
         )
@@ -23,5 +27,13 @@ describe("Testing button", () => {
     it("should have the button text", () => {
         renderButton(buttonLabel)
         expect(screen.getByText(buttonLabel)).toBeInTheDocument()
+    })
+
+    it("should call onClick function when the button is clicked", () => {
+        renderButton(buttonLabel)
+
+        userEvent.click(screen.getByText(buttonLabel))
+
+        expect(mockOnClickFunction).toHaveBeenCalledTimes(1)
     })
 })
