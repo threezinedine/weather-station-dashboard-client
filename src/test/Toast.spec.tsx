6f88ece03f4b95d8fe2a98b3c74ffae7773fc632
unknown <threezinedine@gmail.com>
2023-01-30 17:06:18 +0700

    add test for display notification of the toast

--- src/test/Toast.spec.tsx
+++ src/test/Toast.spec.tsx
@@ -12,20 +12,28 @@ import {
 describe("<Toast /> testing", () => {
     const firstErrorMessage = "First Error Message"
     const secondErrorMessage = "Second Error Message"
+    const notification = "Notification"
     const errorMessages = [firstErrorMessage, secondErrorMessage]
+    const notifications = [notification]
 
     const renderTheToast = () => {
         render(
             <Toast 
                 errorMessages={errorMessages}
+                notifications={notifications}
             />
         ) 
     }
 
-    it("should display all the errorMessages as error class", () => {
+    it("should display all the errorMessages", () => {
         renderTheToast()
 
         expect(screen.getByText(firstErrorMessage)).toBeInTheDocument()
         expect(screen.getByText(secondErrorMessage)).toBeInTheDocument()
+    })
+
+    it("should display the notification", () => {
+        renderTheToast()
+        expect(screen.getByText(notification))
     })
 })
