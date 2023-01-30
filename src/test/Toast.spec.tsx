077579979b809f124376929103c45a1971e2bd79
unknown <threezinedine@gmail.com>
2023-01-30 17:00:46 +0700

    add test for the toast the display all errorMessages into the screen

---
+++ src/test/Toast.spec.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { 
+    render,
+    screen,
+} from "@testing-library/react"
+
+
+describe("<Toast /> testing", () => {
+    const firstErrorMessage = "First Error Message"
+    const secondErrorMessage = "Second Error Message"
+    const errorMessages = [firstErrorMessage, secondErrorMessage]
+
+    const renderTheToast = () => {
+        render(
+            <Toast 
+                errorMessages={errorMessages}
+            />
+        ) 
+    }
+
+    it("should display all the errorMessages as error class", () => {
+        renderTheToast()
+
+        expect(screen.getByText(firstErrorMessage)).toBeInTheDocument()
+        expect(screen.getByText(secondErrorMessage)).toBeInTheDocument()
+    })
+})
