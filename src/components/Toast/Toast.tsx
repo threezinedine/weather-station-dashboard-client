e0200757548210c803c205c408626171a7cfdcfd
unknown <threezinedine@gmail.com>
2023-01-30 17:07:43 +0700

    pass test for display all notifications from the toast

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -5,6 +5,7 @@ import ToastProps from "./ToastProps"
 
 const Toast: React.FC<ToastProps> = ({
     errorMessages,
+    notifications,
 }) => {
     return (
         <div>
@@ -17,6 +18,15 @@ const Toast: React.FC<ToastProps> = ({
                     </div>
                 ))
             } 
+            {
+                notifications.map((notification: string, index: number) => (
+                    <div
+                        key={index}
+                    >
+                        { notification }
+                    </div>
+                )) 
+            }
         </div>
     )
 }
--- src/components/Toast/ToastProps.ts
+++ src/components/Toast/ToastProps.ts
@@ -1,3 +1,4 @@
 export default interface ToastProps {
     errorMessages: string[]
+    notifications: string[]
 }
